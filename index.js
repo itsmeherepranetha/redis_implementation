66a00511d1bada179b328f8e3234d9d466c37e9f
itsmeherepranetha <pranethacoding@gmail.com>
2025-06-10 11:50:09 +0530

    implementing set,get in mem

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const net=require('net');
+const Parser=require('redis-parser');
+
+const store={};
 
 const server=net.createServer(connection=>{
     console.log('client connected...');
 
     connection.on('data',data=>{
-        console.log(data.toString()); // encoded data which follows Redis serialization protocol(RESP)
+
+        //console.log(data.toString()); // encoded data which follows Redis serialization protocol(RESP)
         // set name pranetha , is encoded as ....
         // *3 ---> * for arrays....since 3 strings are there , so size 3
         // $3 ---> $ for each string .... since 'set' is string of size 3
@@ -14,7 +18,32 @@ const server=net.createServer(connection=>{
         // $8 ---> 'pranetha' is a string of size 8
         // pranetha
 
-        connection.write('+OK\r\n'); // now for every query , this message will be sent
+        const parser=new Parser({
+            returnReply:(reply)=>{
+                //console.log('-->',reply); // like "set name pranetha" has reply=['set','name','pranetha']
+                const command=reply[0];
+                if(command=='set')
+                {
+                    const key=reply[1];
+                    const value=reply[2];
+                    store[key]=value;
+                    connection.write('+OK\r\n');
+                }
+                else if(command=='get')
+                {
+                    const key=reply[1];
+                    const value=store[key];
+                    if(!value)connection.write('$-1\r\n'); // output as null (nil)
+                    else connection.write(`$${value.length}\r\n${value}\r\n`) // output as a bulk string
+                }
+            },
+            returnError:(err)=>{
+                console.log('-->',err);
+            }
+        })
+
+        parser.execute(data);
+        //connection.write('+OK\r\n'); // now for every query , this message will be sent
     })
 })
 
